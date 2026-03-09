b688a2be94d10f9c9c96273d63820e0113104bbb
males23 <smed_malela@yahoo.fr>
2026-03-09 15:18:26 +0100

    Conditions et Boucles en C# .NET
    Conditions et Boucles en C# .NET avec quelques exemples

--- branches-loops/obj/Debug/net10.0/branches-loops.AssemblyInfo.cs
+++ branches-loops/obj/Debug/net10.0/branches-loops.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("classe-struct")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("branches-loops")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69f65041a3fe55139d9472127bd0b65da57fe812")]
-[assembly: System.Reflection.AssemblyProductAttribute("classe-struct")]
-[assembly: System.Reflection.AssemblyTitleAttribute("classe-struct")]
+[assembly: System.Reflection.AssemblyProductAttribute("branches-loops")]
+[assembly: System.Reflection.AssemblyTitleAttribute("branches-loops")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
